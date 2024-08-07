93451d1ef70c1f5dbea549e263c3767c6346a665
VishwasBJ <vishwasjai2004@gmail.com>
2024-08-08 01:32:00 +0530

    first
    first

---
+++ script1.js
@@ -0,0 +1,18 @@
+alert("vishwas is coding");
+
+
+console.log("console is hai yaar");
+console.log("sugoi");
+console.log("im hacker");
+console.log("console is mine");
+
+var a = prompt("enetr ur number");
+console.log("ur number is " + a);
+var soWhat =confirm("are i sure u want to ")
+if(soWhat){
+    console.log("suprith  ");
+
+}
+else{
+    console.log("im not "); 
+}

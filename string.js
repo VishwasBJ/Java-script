30798eeea8fea1d61a57bddd142f97dd585175b2
VishwasBJ <vishwasjai2004@gmail.com>
2024-08-09 18:58:24 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,9 @@
+let b="Shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+console.log(b.replace("Sh","Vi"))
+console.log(b.concat("Suprith"))
+

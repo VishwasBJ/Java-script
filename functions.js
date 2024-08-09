9e760e7cbf19e6e12215b508b83d3ffadcd75024
VishwasBJ <vishwasjai2004@gmail.com>
2024-08-09 17:42:23 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,11 @@
+function hoi(a,b) {
+   console.log("the sum is "+(a+b));
+    
+}
+hoi(3,4);
+function nice(name) {
+    console.log("hey "  + name + " is good coder");
+    console.log("hey " + name + " is nice coder");
+    console.log("hey " + name + " is ultimate");
+}
+nice("Vishwas");

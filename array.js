b01c66661756b535a8b0a130e31c7734ed701f16
VishwasBJ <vishwasjai2004@gmail.com>
2024-08-10 23:28:46 +0530

    ..

---
+++ array.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3,4,5];
+//console.log(arr);
+//console.log(arr[0] , typeof arr);
+//arr[0]=5666;
+console.log(arr.toString());
+console.log(arr.join(" and "));
+let a =[1,3,4,5,6];
+a.pop();
+a.push(100);
+a.push("Vishwas");
+a.shift();
+a.unshift("Suprith");
+delete a[2];
+console.log(arr.concat(a[5]))
+arr.splice(1,2)

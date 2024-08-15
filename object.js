fb2aa3294f02a8dcd7c8c982c176cc24d938fa0b
VishwasBJ <vishwasjai2004@gmail.com>
2024-08-16 00:08:34 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,49 @@
+var ourDog = {
+    "name": "Pinku",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything"]
+
+};
+let a = "Vishwas";
+console.log(a + 1);
+console.log(typeof (a));
+const b = 3.14;
+console.log(b);
+let ab = 69;
+let c = ab ** 3;
+console.log(c);
+if (ab % 2 == 0) {
+    console.log("devisible by 2");
+
+}
+else {
+    console.log("not devisible");
+
+}
+(ab > 10) ? 'yes' : 'no'
+
+var lookup = {
+    "alpha": "adams.",
+    "bravo": "boston",
+    charlie: "hoi"
+};
+console.log(lookup.hasOwnProperty());
+
+
+var myMusic = [
+    {
+        "artist":"chinnu",
+        "song":"aha tamatar",
+        "sponsor":"venki",
+        "arrayjs":[
+            "cd",
+            "hoi",
+            "LP"
+        ]
+    }
+];
+console.log(myMusic[0].arrayjs[1]);
+
+
+
